14b32a0299f71a3ae73f68d3d8492e5a52614a91
dustinpfister <dustin.pfister@gmail.com>
2017-02-17 14:59:08 -0500

    gen tile sheet

--- js/main.js
+++ js/main.js
@@ -17,38 +17,7 @@ var app = (function () {
             app.load.spritesheet('button', 'img/button.png', 160, 45);
             app.load.image('tiles', 'img/tiles.png');
 
-            // set up diglayer
-            app.load.tilemap('diglayer', null, {
-                "width" : 5,
-                "height" : 5,
-                "tileheight" : 32,
-                "tilewidth" : 32,
-                "backgroundcolor" : "#656667",
-                "orientation" : "orthogonal",
-                "layers" : [{
-                        "data" : [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                        "height" : 5,
-                        "name" : "layer1",
-                        "opacity" : 1,
-                        "type" : "tilelayer",
-                        "visible" : true,
-                        "width" : 5,
-                        "x" : 0,
-                        "y" : 0
-                    }
 
-                ],
-                "tilesets" : [{
-                        "firstgid" : 0,
-                        "image" : "tiles.png",
-                        "imageheight" : 64,
-                        "imagewidth" : 64,
-                        "name" : "diglayer1",
-                        "tileheight" : 32,
-                        "tilewidth" : 32
-                    }
-                ],
-            }, Phaser.Tilemap.TILED_JSON);
 
         },
 
--- js/title.js
+++ js/title.js
@@ -1,4 +1,55 @@
 
+var genLayer = function () {
+
+    // generate tile map
+
+    var width = 4,
+    height = 4,
+    i = 0,
+    len,
+    data = [],//[Math.floor(Math.random() * 3) + 1, 0, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 0, 0, 4];
+
+    i = 0,
+    len = width * height;
+    while (i < len) {
+
+        data.push(Math.floor(Math.random() * 3 + 1));
+
+        i += 1;
+
+    }
+
+    app.load.tilemap('diglayer', null, {
+        width : width,
+        height : height,
+        tileheight : 32,
+        tilewidth : 32,
+        orientation : "orthogonal",
+        layers : [{
+                data : data,
+                height : width,
+                width : height,
+                name : "layer1",
+                type : "tilelayer"
+            }
+
+        ],
+        tilesets : [{
+                firstgid : 1,
+                image : "tiles.png",
+                imageheight : 64,
+                imagewidth : 64,
+                name : "diglayer1",
+                tileheight : 32,
+                tilewidth : 32
+            }
+        ],
+    }, Phaser.Tilemap.TILED_JSON);
+
+},
+
+layer;
+
 var Title = function () {},
 
 map,
@@ -18,22 +69,37 @@ proto.create = function () {
     logo.x = app.width / 2 - logo.width / 2;
     logo.y = app.world.centerY - 200;
 
-    //tiles = app.add.sprite(0, 0, 'tiles');
-
-    //map = app.add.tilemap('digLayer');
+    genLayer();
 
     map = app.add.tilemap('diglayer');
     map.addTilesetImage('diglayer1', 'tiles');
+
+    //var tile = map.layers[0].data[0][0];
+    //tile.alpha = 0;
+
+    //console.log(map.layers[0].data[0][0]);
+
     layer = map.createLayer('layer1');
+
+    console.log(map.layers);
 
     //  This resizes the game world to match the layer dimensions
     //layer.resizeWorld();
 
     app.add.button(app.world.centerX - 80, app.world.centerY + 30, 'button', function () {
 
-        console.log('yeah man I was clicked what up?');
-
         app.state.start('game');
+
+    }, this, 0, 0, 1);
+
+    app.add.button(app.world.centerX - 80, app.world.centerY + 100, 'button', function () {
+
+        console.log('re do the layer');
+
+        genLayer();
+        map = app.add.tilemap('diglayer');
+        map.addTilesetImage('diglayer1', 'tiles');
+        map.createLayer('layer1');
 
     }, this, 0, 0, 1);
 
