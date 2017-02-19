0c4490562333c5dc51f165db38dc6acb9831ca52
dustinpfister <dustin.pfister@gmail.com>
2017-02-19 11:46:38 -0500

    made title state just an object that is returned rather then a function and prototype

--- js/title.js
+++ js/title.js
@@ -1,73 +1,81 @@
 
-var Title = function () {},
 
-proto = Title.prototype;
+var Title = (function () {
 
-proto.create = function () {
+    var map,
+    layer1;
 
-    //  Creates a blank tilemap
-    var map = app.add.tilemap();
+    return {
 
-    //  Add a Tileset image to the map
-    map.addTilesetImage('tiles');
+        create : function () {
 
-    layer1 = map.create('level1', 4, 4, 32, 32);
-    layer1.setScale(3.5, 3.5);
-    layer1.fixedToCamera = false;
-    layer1.x = app.world.centerX - (32 * 2) * 3.5;
-    layer1.y = app.world.centerY - (32 * 2) * 3.5;
+            //  Creates a blank tilemap
+            var map = app.add.tilemap();
 
-    var genLayer = function () {
+            //  Add a Tileset image to the map
+            map.addTilesetImage('tiles');
 
-        var width = 4,
-        height = 4,
-        i = 0,
-        x,
-        y,
-        len,
-        data = [],
-        len = width * height;
+            console.log('title state setup.');
 
-        // use map.put to populate the layer
-        while (i < len) {
+            layer1 = map.create('level1', 4, 4, 32, 32);
+            layer1.setScale(3.5, 3.5);
+            layer1.fixedToCamera = false;
+            layer1.x = app.world.centerX - (32 * 2) * 3.5;
+            layer1.y = app.world.centerY - (32 * 2) * 3.5;
 
-            x = i % width;
-            y = Math.floor(i / width);
+            var genLayer = function () {
 
-            map.putTile(
-
-                Math.floor(Math.random() * 4),
+                var width = 4,
+                height = 4,
+                i = 0,
                 x,
                 y,
-                'level1');
+                len,
+                data = [],
+                len = width * height;
 
-            i += 1;
+                // use map.put to populate the layer
+                while (i < len) {
+
+                    x = i % width;
+                    y = Math.floor(i / width);
+
+                    map.putTile(
+
+                        Math.floor(Math.random() * 4),
+                        x,
+                        y,
+                        'level1');
+
+                    i += 1;
+
+                }
+
+            };
+
+            genLayer();
+
+            app.add.button(app.world.centerX - 80, app.world.centerY + 30, 'button', function () {
+
+                app.state.start('game');
+
+            }, this, 0, 0, 1);
+
+            app.add.button(app.world.centerX - 80, app.world.centerY + 100, 'button', function () {
+
+                genLayer();
+
+            }, this, 0, 0, 1);
+
+            var logo = app.add.sprite(app.world.centerX, app.world.centerY, 'logo');
+            logo.angle = 0;
+            logo.width = logo.width * 2;
+            logo.height = logo.height * 2;
+            logo.x = app.width / 2 - logo.width / 2;
+            logo.y = app.world.centerY - 200;
 
         }
-
     };
 
-    genLayer();
-
-    app.add.button(app.world.centerX - 80, app.world.centerY + 30, 'button', function () {
-
-        app.state.start('game');
-
-    }, this, 0, 0, 1);
-
-    app.add.button(app.world.centerX - 80, app.world.centerY + 100, 'button', function () {
-
-        genLayer();
-
-    }, this, 0, 0, 1);
-
-    var logo = app.add.sprite(app.world.centerX, app.world.centerY, 'logo');
-    logo.angle = 0;
-    logo.width = logo.width * 2;
-    logo.height = logo.height * 2;
-    logo.x = app.width / 2 - logo.width / 2;
-    logo.y = app.world.centerY - 200;
-
-};
-
-proto.update = function () {};
+}
+    ());
