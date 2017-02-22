056991776a059ce9b89257cd96e40f4ef1b2eae9
dustinpfister <dustin.pfister@gmail.com>
2017-02-22 18:23:01 -0500

    some title.js cleanup

--- js/title.js
+++ js/title.js
@@ -12,32 +12,7 @@ var Title = (function () {
 
             var button,
             scale,
-            genLayer,
-            logo;
-
-            //  Creates a blank tilemap
-            map = app.add.tilemap();
-
-            //  Add a Tileset image to the map
-            map.addTilesetImage('tiles');
-
-            console.log('title state setup.');
-
-            layer1 = map.create('level1', 4, 4, 32, 32);
-
-            //layer1.setScale(3.5, 3.5);
-            //layer1.fixedToCamera = false;
-            //layer1.x = app.world.centerX - (32 * 2) * 3.5;
-            //layer1.y = app.world.centerY - (32 * 2) * 3.5;
-
-            //layer1.setScale(1, 1);
-            layer1.width = app.height;
-            layer1.height = app.height;
-            layer1.fixedToCamera = false;
-            layer1.x = app.world.centerX - (layer1.width / 2); //app.world.centerX - (32 * 2) * 3.5;
-            layer1.y = 0; //app.world.centerY - (32 * 2) * 3.5;
-
-
+            logo,
             genLayer = function () {
 
                 var width = 4,
@@ -68,31 +43,36 @@ var Title = (function () {
 
             };
 
+            //  Creates a blank tilemap
+            map = app.add.tilemap();
+
+            //  Add a Tileset image to the map
+            map.addTilesetImage('tiles');
+
+            layer1 = map.create('level1', 4, 4, 32, 32);
+            layer1.width = app.height;
+            layer1.height = app.height;
+            layer1.fixedToCamera = false;
+            layer1.x = app.world.centerX - (layer1.width / 2); //app.world.centerX - (32 * 2) * 3.5;
+            layer1.y = 0; //app.world.centerY - (32 * 2) * 3.5;
+
+            // generate the background image layer
             genLayer();
 
             // new game button
-            //var button = app.add.button(app.world.centerX - 80, app.world.centerY + 30, 'button', function () {
             button = app.add.button(0, 0, 'button', function () {
 
                     app.state.start('dig_run');
 
                 }, this, 0, 0, 1);
 
-            // scale to game size
+            // scale button to game size
             button.width = 1.60 * (app.width / 4);
             button.height = .45 * (app.width / 4);
             button.x = app.world.centerX - (button.width / 2);
             button.y = app.world.centerY + (app.height * 0.125);
 
-            /*
-            logo = app.add.sprite(app.world.centerX, app.world.centerY, 'logo');
-            logo.angle = 0;
-            logo.width = logo.width * 2;
-            logo.height = logo.height * 2;
-            logo.x = app.width / 2 - logo.width / 2;
-            logo.y = app.world.centerY - 200;
-             */
-
+            // the game logo
             logo = app.add.sprite(app.world.centerX, app.world.centerY, 'logo');
             logo.angle = 0;
             logo.width = 2 * (app.width / 3); //logo.width * 2;
