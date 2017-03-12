09587bb10af6fa7d0ed89d022a0ec09186c8f101
dustinpfister <dustin.pfister@gmail.com>
2017-03-12 17:22:35 -0400

    using js-beautify

--- pre-commit/bumpversion.js
+++ pre-commit/bumpversion.js
@@ -1,15 +1,22 @@
+
+var fs = require('fs'),
+beautify = require('js-beautify').js_beautify;
+
 console.log('bumpversion.js:');
 
-var fs = require('fs');
-
+// read package.json
 fs.readFile('package.json', 'utf8', function (err, data) {
 
     if (err) {
+
+        // if error geting package.json
 
         console.log('error reading package.json');
         console.log(err);
 
     } else {
+
+        // reading package.json
 
         var pack = JSON.parse(data),
         versionNumber = pack.version.split('.');
@@ -30,7 +37,7 @@ fs.readFile('package.json', 'utf8', function (err, data) {
         pack.version = versionNumber;
 
         // write the change
-        fs.writeFile('package.json', JSON.stringify(pack), 'utf8', function (err) {
+        fs.writeFile('package.json', beautify(JSON.stringify(pack), { indent_size: 2 }), 'utf8', function (err) {
 
             if (err) {
 
